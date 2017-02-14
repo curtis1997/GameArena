68c9f4d68e803f0f9d392120cfc7be5074c3290c
Curtis <c.dransfield@lancaster.ac.uk>
2017-02-14 18:05:11 +0000

    Started creating balls that drop from sky

--- Driver.java
+++ Driver.java
@@ -13,7 +13,25 @@ public class Driver {
 	 String ballColour = "Red";
 	 GameArena Arena = new GameArena(Width, Height);
 	 Ball ball1 = new Ball(Width/2, Height/2, Diameter, ballColour);
-	 Arena.addBall(ball1);
+	 
+	 Ball[] balls = new Ball[2];
+  
+	 
+	     for (int i = 0 ; i < balls.length; i++)
+		 {
+			    for (int j = 0; j<100; j++)
+					Arena.pause();
+                balls[i] = new Ball(Math.random()*Width, 10, 10, "GREEN");
+				Arena.addBall(balls[i]);
+			
+				  for (int p = 0; p<i; p++)
+				  {
+				  balls[p].movedown(30);
+				  for (int j = 0; j<100; j++)
+				  	Arena.pause();
+				  }
+		 }
+	 
 	 for(;;)
 	 {		 
 		 Arena.pause();
