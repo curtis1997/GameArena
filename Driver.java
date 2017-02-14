64377b0ba016c3efbe04509f17f797b63f074e7e
Curtis <c.dransfield@lancaster.ac.uk>
2017-02-14 19:20:13 +0000

    Balls now drop correctly from top of screen

--- Driver.java
+++ Driver.java
@@ -3,49 +3,37 @@ public class Driver {
  public static void main(String[] arguments)
  {
 	 int Width = 800;
-	 int Height = 400;
+	 int Height = 500;
 	 int Diameter = 10;
-	 boolean switchX = false;
-     boolean switchY = false;
-	 int randomx = (int)(Math.random()*10);
-	 int randomy = (int)(Math.random()*10);
+	 int NumberOfBalls = 4;
+	 int i = 0;
+	 int Speed = 3;
 	 
 	 String ballColour = "Red";
 	 GameArena Arena = new GameArena(Width, Height);
-	 Ball ball1 = new Ball(Width/2, Height/2, Diameter, ballColour);
 	 
-	 Ball[] balls = new Ball[2];
+	 Ball[] balls = new Ball[NumberOfBalls];
   
 	 
-	     for (int i = 0 ; i < balls.length; i++)
+	     for (i = 0 ; i < balls.length; i++)
 		 {
-			    for (int j = 0; j<100; j++)
-					Arena.pause();
-                balls[i] = new Ball(Math.random()*Width, 10, 10, "GREEN");
+			    
+                balls[i] = new Ball(Math.random()*(Width-Diameter), 10, 10, "GREEN");
 				Arena.addBall(balls[i]);
 			
-				  for (int p = 0; p<i; p++)
-				  {
-				  balls[p].movedown(30);
-				  for (int j = 0; j<100; j++)
-				  	Arena.pause();
-				  }
+			     for (int j = 0; j<(((Height/2)+3)/Speed); j++)
+				 {
+				   for (int p = 0; p<i+1; p++)
+				   balls[p].movedown(Speed);
+			       Arena.pause();
+				 }
 		 }
-	 
-	 for(;;)
-	 {		 
-		 Arena.pause();
-		 if(ball1.getXPosition() > (Width-Diameter))
-		     ball1.setXPosition(ball1.getXPosition()-randomx);
-	     else
-			ball1.setXPosition(ball1.getXPosition()+randomx);
-		
-		if (ball1.getYPosition() > (Height-Diameter))
-		     ball1.setYPosition(ball1.getYPosition()-randomy);
-	    else
-			ball1.setYPosition(ball1.getYPosition()+randomy);
+		 for (int j = 0; j<200; j++)
+			{
+			balls[i-1].movedown(Speed);
+			Arena.pause();
+			}
 	
-	 }
  }
 
 }
